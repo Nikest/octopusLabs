1dda8e514ab5036aeea6e4e3b6f012a8d80e8e22
Nikest <alsn89@gmail.com>
2019-01-18 11:42:35 +0200

    Logo fix

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { sl } from 'Services/Styles';
 
 export const Logo = function () {
-  const c = sl(() => require('./logo.scss'));
+  const c = sl(() => require('./Logo.scss'));
 
   return (
     <div className={c('container')}>
